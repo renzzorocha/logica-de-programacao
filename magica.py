4f46cc3edeae89f2286a2175762c54127678602a
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-11 14:46:25 -0300

    feat: magic wooah

---
+++ magica.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('clear')
+
+print('Olhe as cartas abaixo e escolha uma')
+print()
+print('Q♣️  K♦️  J♥️  Q♥️  J♣️  K♠️')
+input()
+os.system('clear')
+
+print('A carta que você pensou...')
+input()
+print('Desapareceu!')
+print()
+print('J♠️  Q♦️  J♦️  Q♠️  K♥️')

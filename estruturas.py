c18734d2a1fd0123be1a78f975f112785f6c39ba
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-13 11:50:43 -0300

    fix: commit fix

---
+++ estruturas.py
@@ -0,0 +1,25 @@
+# - Listas: conjunto de dados ordenados - mutável
+lista = [1, 4, 3, 4]
+frutas = ['banana', 'maçã']
+mista = [1, 'texto', 5, True]
+
+# - Tuplas: conjunto de dados ordenados - imutável
+coordenada = (5, 6)
+tupla = (5, 4, 3, 2)
+
+# Dicionários (dict): conjunto de pares na forma chave e valor
+pessoa = { 'nome': 'João', 'idade': 18 }
+carro = {
+    'marca': 'Volks',
+    'modelo': 'Fusca',
+    'cor': 'azul',
+    'ano': 1970
+}
+
+# Conjuntos (sets): Conjunto de dados não ordenados e sem repetição
+c1 = {1, 2, 3}
+c2 = {3, 2, 1}
+c3 = {1, 2, 2, 2, 2, 2, 3, 3, 2, 1, 2, 3}
+
+dir(carro)
+

3267db2d1bff9a222e5c17cb26985cfd3cea6ab5
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-27 16:15:24 -0300

    fix: arrumando fila com deque

--- projetos/fila.py
+++ projetos/fila.py
@@ -1,8 +1,24 @@
+from collections import deque
+
 def criarFila():
-    return []
+    return deque()
 
 def inserirNaFila(fila, elemento):
     fila.append(elemento)
 
 def removerDaFila(fila):
-    fila.pop(0)
+    fila.popleft()
+
+fila = criarFila()
+
+inserirNaFila(fila, 4)
+inserirNaFila(fila, 5)
+inserirNaFila(fila, 6)
+inserirNaFila(fila, 7)
+inserirNaFila(fila, 8)
+inserirNaFila(fila, 9)
+inserirNaFila(fila, 0)
+removerDaFila(fila)
+removerDaFila(fila)
+removerDaFila(fila)
+print(fila)

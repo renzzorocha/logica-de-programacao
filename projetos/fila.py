a93d6e0a03d43a597e0db65560d12bf3a7a84ce9
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-27 16:07:30 -0300

    feat: adicionando fila em python

--- projetos/fila.py
+++ projetos/fila.py
@@ -6,15 +6,3 @@ def inserirNaFila(fila, elemento):
 
 def removerDaFila(fila):
     fila.pop(0)
-
-fila = criarFila()
-
-inserirNaFila(fila, 5)
-inserirNaFila(fila, 7)
-inserirNaFila(fila, 8)
-inserirNaFila(fila, 12)
-inserirNaFila(fila, 15)
-inserirNaFila(fila, 18)
-inserirNaFila(fila, 33)
-removerDaFila(fila)
-print(fila)
